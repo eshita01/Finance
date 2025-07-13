2e408ec5d9d3c64bdb3b2764c6d26d31f52bc65d
Eshita Sohani <44199998+eshita01@users.noreply.github.com>
2025-07-13 18:52:05 +0100

    Add peer company analysis pipeline

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,10 +1,12 @@
 from .technical_analysis import compute_indicators, analyze
 from .sentiment_analysis import analyze as analyze_sentiment
 from .insider_analysis import analyze as analyze_insider
+from .peer_analysis import analyze as analyze_peers
 
 __all__ = [
     'compute_indicators',
     'analyze',
     'analyze_sentiment',
     'analyze_insider',
+    'analyze_peers',
 ]
--- data_sources/__init__.py
+++ data_sources/__init__.py
@@ -1,9 +1,11 @@
 from .stock_data_fetcher import StockDataFetcher
 from .news_sentiment_fetcher import NewsSentimentFetcher
 from .insider_data_fetcher import InsiderDataFetcher
+from .peer_data_fetcher import PeerDataFetcher
 
 __all__ = [
     'StockDataFetcher',
     'NewsSentimentFetcher',
     'InsiderDataFetcher',
+    'PeerDataFetcher',
 ]
